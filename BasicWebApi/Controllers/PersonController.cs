0771bf429687e02a0082074b3680bb86c05e53b9
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2018-04-19 09:17:28 +0200

    Zajecia w marcu

--- BasicWebApi/Controllers/PersonController.cs
+++ BasicWebApi/Controllers/PersonController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace BasicWebApi.Controllers
@@ -15,14 +16,27 @@ namespace BasicWebApi.Controllers
         // GET: api/Person
         public IEnumerable<Person> Get()
         {
-            return phoneBook.persons;
+            return phoneBook.GetPersons();
 
         }
 
+        /// <summary>
+        /// Looks up persons from phonebook by ID.
+        /// </summary>
+        /// <param name="id">The ID of the person to be retrived.</param>
         // GET: api/Person/5
         public Person Get(int id)
         {
-            return phoneBook.persons[id];
+            Person p = phoneBook.GetPersonWithID(id);
+            if (p == null)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent("Unable to find any results")
+                };
+                throw new HttpResponseException(response);
+            }
+            return p;
         }
 
         // POST: api/Person
@@ -30,37 +44,37 @@ namespace BasicWebApi.Controllers
         {
             Debug.WriteLine("Zapytanie Post");
             Debug.WriteLine(person.Name);
-            //Należało by sprawdzić czy osoba istnieje w phonebook
-            lock (phoneBook) {
-                int id = phoneBook.persons.Count;
-                person.ID = id;
-                phoneBook.persons.Add(person);
+            if (!phoneBook.AddNewPerson(person))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent("Unable to find any results")
+                };
+                throw new HttpResponseException(response);
             }
             return person;
+            
         }
 
         // PUT: api/Person/5
         public Person Put(int id, [FromBody]Person person)
         {
-            Person x;
-            lock (phoneBook) { 
-                x = phoneBook.persons[id];
-                x.Name = person.Name;
-                x.Organization = person.Organization;
-                x.Phones = person.Phones;
+            if (!phoneBook.UpdatePersonWithID(id, person))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent("Unable to find any results")
+                };
+                throw new HttpResponseException(response);
             }
-            return x;
+            return person;
         }
 
         // DELETE: api/Person/5
         public Person Delete(int id)
         {
-            Person x;
-            lock (phoneBook)
-            {
-                x = phoneBook.persons[id];
-            }
-            return x;
+            
+            return phoneBook.DeletePerson(id);
         }
     }
 }
--- BasicWebApi/Models/PhoneBook.cs
+++ BasicWebApi/Models/PhoneBook.cs
@@ -12,25 +12,32 @@ namespace BasicWebApi.Models
 
         public PhoneBook()
         {
+            phones = new List<Phone>() {
+                new Phone {ID=0, Number="121212"},
+                new Phone {ID=1, Number="33121212"},
+                new Phone {ID=2, Number="11142"},
+                new Phone {ID=3, Number="222333"}
+            };
+
             persons = new List<Person>()
             {
               new Person {
                   ID = 0, Name = "Adam", Organization = "ZUS",
                   Phones=new List<Phone>{
-                      new Phone {ID=0, Number="121212"}
+                      phones[0]
                   }
               },
               new Person {
                   ID = 1, Name = "Alicja", Organization = "ZUS",
                   Phones=new List<Phone>{
-                      new Phone {ID=1, Number="33121212"}
+                      phones[1]
                   }
               },
               new Person {
                   ID = 2, Name = "Dagmara", Organization = "ZUS",
                   Phones=new List<Phone>{
-                      new Phone {ID=2, Number="11142"},
-                      new Phone {ID=3, Number="222333"}
+                      phones[2],
+                      phones[3]
                   }
               }
             };
@@ -39,26 +46,87 @@ namespace BasicWebApi.Models
 
         public List<Person> GetPersons()
         {
-            return persons.Select(p => (Person) p.Clone()).ToList();
+            lock (persons)
+            {
+                return persons.Select(p => (Person)p.Clone()).ToList();
+            }
         }
 
         public Person GetPersonWithID(int id)
         {
-            return (Person) persons[id].Clone();
+            lock (persons)
+            {
+                if (id < persons.Count) return (Person)persons[id].Clone();
+                else return null;
+            }
         }
 
-        public Person AddNewPerson(Person person) { 
-            return null;
+        public bool AddNewPerson(Person person) {
+
+            AddOrUpdatePhones(person.Phones);
+
+            lock (persons)
+            {
+                int personId = persons.IndexOf(person);
+                person.ID = personId;
+            
+                if (persons.Contains(person))
+                {
+                    return UpdatePersonWithID((int)person.ID, person);
+                }
+                        
+                int id = persons.Count;
+                person.ID = id;
+                persons.Add((Person) person.Clone());
+            }
+
+            return true;
         }
 
-        public Person UpdatePersonWithID(int id, Person person)
+        private void AddOrUpdatePhones(List<Phone> ph)
         {
-            return null;
+            foreach (Phone p in ph)
+            {
+                lock (phones)
+                {
+                    int id = ph.IndexOf(p);
+                    if (id == -1)
+                    {
+                        int pid = phones.Count;
+                        p.ID = id;
+                        phones.Add((Phone)p.Clone());
+                    }
+                    else
+                    {
+                        p.ID = id;
+                    }
+                }
+            }
+        }
+
+        public bool UpdatePersonWithID(int id, Person person)
+        {
+            AddOrUpdatePhones(person.Phones);
+            Person x;
+            lock (persons)
+            {
+                x = persons[id];
+                x.Name = person.Name;
+                x.Organization = person.Organization;
+                x.Phones = person.Phones;
+            }
+            return true;
         }
 
         public Person DeletePerson(int id)
         {
-            return null;
+            Person x;
+            lock (persons)
+            {
+                x = persons[id];
+                persons.RemoveAt(id);
+            }
+            return x;
         }
 
     }
