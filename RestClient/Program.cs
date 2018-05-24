324a19aaf71502c60ec44f6a99ff119db4490944
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2018-05-24 09:55:58 +0200

    small refactoring, RestCllient created

--- RestClient/Program.cs
+++ RestClient/Program.cs
@@ -11,59 +11,7 @@ namespace RestClient
 {
     class Program
     {
-        static HttpClient client = new HttpClient();
-
-        static async Task<String> CreatePersonAsync(Person person)
-        {
-            Console.WriteLine(person.AsJSON());
-
-            HttpContent content = new StringContent(person.AsJSON(),
-                                    Encoding.UTF8,
-                                    "application/json");
-            //content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            HttpResponseMessage response = await client.PostAsync(
-                "api/Person", content);
-            response.EnsureSuccessStatusCode();
-
-            // return URI of the created resource.
-            return response.Content.ToString();
-        }
-
-        static async Task<String> GetPersonAsync(string path)
-        {
-            Person product = null;
-            String sPerson = null;
-            HttpResponseMessage response = await client.GetAsync(path);
-            if (response.IsSuccessStatusCode)
-            {
-                sPerson = await response.Content.ReadAsStringAsync();
-            }
-            //Do Dorobienia Konwersja json na Person i zwóc Person 
-            return sPerson;
-        }
-
         
-        static async Task<String> UpdatePersonAsync(Person person)
-        {
-            HttpContent content = new StringContent(person.AsJSON(),
-                                    Encoding.UTF8,
-                                    "application/json");
-            HttpResponseMessage response = await client.PutAsync(
-                $"api/Person/{person.ID}", content);
-            response.EnsureSuccessStatusCode();
-
-            // Deserialize the updated product from the response body.
-            String s = await response.Content.ReadAsStringAsync();
-            return s;
-        }
-
-        
-        static async Task<HttpStatusCode> DeletePersonAsync(string id)
-        {
-            HttpResponseMessage response = await client.DeleteAsync(
-                $"api/Person/{id}");
-            return response.StatusCode;
-        }
 
         
         static void Main(string[] args)
@@ -74,11 +22,8 @@ namespace RestClient
         static async Task RunAsync()
         {
             // Update port # in the following line.
-            client.BaseAddress = new Uri("http://localhost:39967/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
+            RestClient client = RestClient.NewInstance("http://localhost:39967/");
+            
             try
             {
                 // Create a new product
@@ -95,12 +40,12 @@ namespace RestClient
                     Organization = "TVP",
                     Phones = phones
                 };
-                var url = await CreatePersonAsync(p);
+                var url = await client.CreatePersonAsync(p);
                 Console.WriteLine($"Created at {url}");
 
                 
                 // Get the product
-                String s = await GetPersonAsync("api/Person");
+                String s = await client.GetPersonAsync("api/Person");
                 Console.WriteLine("Response ");
                 Console.WriteLine(s);
                 
@@ -108,20 +53,20 @@ namespace RestClient
                 Console.WriteLine("Updating Person Organization...");
                 p.Organization = "Polsat";
                 p.ID = 3;
-                await UpdatePersonAsync(p);
+                await client.UpdatePersonAsync(p);
 
                 // Get the updated product
-                s = await GetPersonAsync("api/Person/3");
+                s = await client.GetPersonAsync("api/Person/3");
                 Console.WriteLine("Response ");
                 Console.WriteLine(s);
 
                 
                 // Delete the product
-                var statusCode = await DeletePersonAsync("0");
+                var statusCode = await client.DeletePersonAsync("0");
                 Console.WriteLine($"Deleted (HTTP Status = {(int)statusCode})");
 
                 // Get All products
-                s = await GetPersonAsync("api/Person");
+                s = await client.GetPersonAsync("api/Person");
                 Console.WriteLine("After Delete ");
                 Console.WriteLine(s);
             }
---
+++ RestClient/RestClient.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClient
+{
+    class RestClient
+    {
+        private HttpClient client = new HttpClient();
+
+        public async Task<String> CreatePersonAsync(Person person)
+        {
+            Console.WriteLine(person.AsJSON());
+
+            HttpContent content = new StringContent(person.AsJSON(),
+                                    Encoding.UTF8,
+                                    "application/json");
+            //content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await client.PostAsync(
+                "api/Person", content);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Content.ToString();
+        }
+
+        public async Task<String> GetPersonAsync(string path)
+        {
+            Person product = null;
+            String sPerson = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                sPerson = await response.Content.ReadAsStringAsync();
+            }
+            //Do Dorobienia Konwersja json na Person i zwóc Person 
+            return sPerson;
+        }
+
+
+        public async Task<String> UpdatePersonAsync(Person person)
+        {
+            HttpContent content = new StringContent(person.AsJSON(),
+                                    Encoding.UTF8,
+                                    "application/json");
+            HttpResponseMessage response = await client.PutAsync(
+                $"api/Person/{person.ID}", content);
+            response.EnsureSuccessStatusCode();
+
+            // Deserialize the updated product from the response body.
+            String s = await response.Content.ReadAsStringAsync();
+            return s;
+        }
+
+
+        public async Task<HttpStatusCode> DeletePersonAsync(string id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync(
+                $"api/Person/{id}");
+            return response.StatusCode;
+        }
+
+        public static RestClient NewInstance(String url)
+        {
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            return this;
+        }
+    }
+}
